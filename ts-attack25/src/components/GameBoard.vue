e685b5e3296d4d4548d617ddcab104f905734048
phantomboyA <phantomboyA@gmail.com>
2023-06-05 14:15:43 +0900

    25マスの表示

---
+++ ts-attack25/src/components/GameBoard.vue
@@ -0,0 +1,25 @@
+<!-- GameBoard.vue -->
+<template>
+    <div class="game-board">
+        <GameSquare v-for="i in 25" :key="i" />
+    </div>
+</template>
+
+<script>
+import GameSquare from './GameSquare.vue';
+
+export default {
+    components: {
+        GameSquare
+    }
+};
+</script>
+
+<style scoped>
+.game-board {
+    display: grid;
+    grid-template-columns: repeat(5, 1fr);
+    grid-template-rows: repeat(5, 1fr);
+    gap: 0;
+}
+</style>
---
+++ ts-attack25/src/components/GameSquare.vue
@@ -0,0 +1,14 @@
+<!-- GameSquare.vue -->
+<template>
+    <div class="game-square"></div>
+</template>
+
+<style scoped>
+.game-square {
+    width: 100%;
+    padding-bottom: 100%;
+    position: relative;
+    border: 1px solid black;
+    box-sizing: border-box;
+}
+</style>
