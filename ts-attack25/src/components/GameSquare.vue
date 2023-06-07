2273bf6e6b19196009c6778a3e58f824f5c9e88c
phantomboyA <phantomboyA@gmail.com>
2023-06-08 03:59:23 +0900

    数字の大きさ調整

--- ts-attack25/src/components/GameSquare.vue
+++ ts-attack25/src/components/GameSquare.vue
@@ -1,68 +1,82 @@
 <!-- GameSquare.vue -->
 <template>
-        <div class="game-square" :style="{ backgroundColor: panelColor }">
-            <div class="number-container">
-                    <div class="number">{{ number }}</div>
-                </div>
+  <div class="game-square" :style="{ backgroundColor: panelColor }" ref="square">
+    <div class="number-container">
+      <div class="number" :style="{ fontSize: fontSize }">{{ number }}</div>
     </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 import { PanelColor } from '../domain/panel';
 
 export default defineComponent({
-    props: {
-        number: {
-            type: Number,
-            required: true
-        },
-        color: {
-            type: Number,
-            required: true
-        }
+  props: {
+    number: {
+      type: Number,
+      required: true
     },
-    computed: {
-        panelColor() {
-            switch (this.color) {
-                case PanelColor.RED:
-                    return 'red';
-                case PanelColor.GREEN:
-                    return 'green';
-                case PanelColor.BLUE:
-                    return 'blue';
-                case PanelColor.WHITE:
-                    return 'white';
-                case PanelColor.GRAY:
-                default:
-                    return 'lightgray';
-            }
-        }
+    color: {
+      type: Number,
+      required: true
     }
+  },
+  setup() {
+    const square = ref<HTMLElement | null>(null);
+    const fontSize = ref('1em');
+
+    onMounted(() => {
+      if (square.value) {
+        fontSize.value = `${square.value.clientHeight * 0.6}px`;
+      }
+    });
+
+    return {
+      square,
+      fontSize
+    };
+  },
+  computed: {
+    panelColor() {
+      switch (this.color) {
+        case PanelColor.RED:
+          return 'red';
+        case PanelColor.GREEN:
+          return 'green';
+        case PanelColor.BLUE:
+          return 'blue';
+        case PanelColor.WHITE:
+          return 'white';
+        case PanelColor.GRAY:
+        default:
+          return 'lightgray';
+      }
+    }
+  }
 });
 </script>
 
 <style scoped>
 .game-square {
-    width: 100%;
-    aspect-ratio: 1 / 1;
-    /* Maintain the aspect ratio as 1:1 */
-    position: relative;
-    border: 1px solid black;
-    box-sizing: border-box;
+  width: 100%;
+  aspect-ratio: 1 / 1;
+  /* Maintain the aspect ratio as 1:1 */
+  position: relative;
+  border: 1px solid black;
+  box-sizing: border-box;
 }
 
 .number-container {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .number {
-    position: relative;
-    font-size: 5vw;
+  position: relative;
 }
 </style>
