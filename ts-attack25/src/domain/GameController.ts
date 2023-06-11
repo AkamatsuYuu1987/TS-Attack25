c9011b712b66efd5aecaaa96c446111f69408c01
phantomboyA <phantomboyA@gmail.com>
2023-06-11 09:15:39 +0900

    パネルの色の変更に応じて、カウンターの数を変更

--- ts-attack25/src/domain/GameController.ts
+++ ts-attack25/src/domain/GameController.ts
@@ -1,6 +1,6 @@
 import GameBoard from './GameBoard';
 import ColorCounter from './ColorCounter';
-import { Panel, PanelColor } from './panel';  // Fixed the import here
+import { Panel, PanelColor } from './panel';
 
 class GameController {
     gameBoard: GameBoard;
@@ -12,6 +12,10 @@ class GameController {
         this.colorCounters = colorCounters;
     }
 
+    setGameBoard(gameBoard: GameBoard): void {
+        this.gameBoard = gameBoard;
+    }
+
     selectColor(color: PanelColor): void {
         this.selectedColor = color;
     }
@@ -20,13 +24,29 @@ class GameController {
         if (this.selectedColor === null) {
             throw new Error('No color has been selected');
         }
-        const row = Math.floor(panelNumber / this.gameBoard.getBoard().length);
-        const col = panelNumber % this.gameBoard.getBoard().length;
+
+        // Get the panel to be changed
+        const panel = this.gameBoard.getBoard().flat().find(p => p.getNumber() === panelNumber);
+        if (!panel) {
+            throw new Error('Panel not found');
+        }
+
+        // Decrement the counter of the current panel's color, unless it's gray
+        const currentColorCounter = this.colorCounters.find(counter => counter.getColor() === panel.getColor());
+        if (currentColorCounter && panel.getColor() !== PanelColor.GRAY) {
+            currentColorCounter.decrement();
+        }
 
         // Clone the gameBoard and apply the color change
         const newGameBoard = new GameBoard(this.gameBoard.getBoard().length, this.gameBoard.getBoard()[0].length);
         newGameBoard.setBoard(this.gameBoard.getBoard().map(row => row.map(panel => new Panel(panel.getColor(), panel.getNumber()))));
         newGameBoard.changeColor(panelNumber, this.selectedColor);
+
+        // Increment the counter of the new panel's color
+        const newColorCounter = this.colorCounters.find(counter => counter.getColor() === this.selectedColor);
+        if (newColorCounter) {
+            newColorCounter.increment();
+        }
 
         return newGameBoard;
     }
