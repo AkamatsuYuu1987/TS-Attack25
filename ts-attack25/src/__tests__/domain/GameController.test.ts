e78a7d3c5bc947a851888fb7709698b9a1280ff7
phantomboyA <phantomboyA@gmail.com>
2023-06-13 12:18:27 +0900

    挟んだパネルの色が変わる機能を実装

--- ts-attack25/src/__tests__/domain/GameController.test.ts
+++ ts-attack25/src/__tests__/domain/GameController.test.ts
@@ -40,14 +40,14 @@ describe('GameController', () => {
     it('should update the color counters when a panel color is changed, excluding gray', async () => {
         // Set panel 12 to red
         gameController.selectColor(PanelColor.RED);
-        let gameBoard = gameController.selectPanel(12);
+        let gameBoard = await gameController.selectPanel(12);
 
         // Save the new gameBoard state to the gameController
         gameController.setGameBoard(gameBoard);
 
         // Now change panel 12 to green
         gameController.selectColor(PanelColor.GREEN);
-        gameBoard = gameController.selectPanel(12);
+        gameBoard = await gameController.selectPanel(12);
 
         // Verify that the red counter has decreased to 0 and the green counter has increased to 1
         const redCounter = gameController.colorCounters.find(counter => counter.getColor() === PanelColor.RED);
@@ -60,5 +60,35 @@ describe('GameController', () => {
 
     it('should throw an error if a panel is selected before a color', () => {
         expect(() => gameController.selectPanel(12)).toThrow('No color has been selected');
+    });
+
+    // Test the flipping panels logic
+    it('should flip the color of panels between selected panel and another panel of the same color', () => {
+        // Initialize the gameBoard to the specific state
+        const initialLayout = [
+            [new Panel(PanelColor.GREEN, 1, 0, 0), new Panel(PanelColor.GRAY, 2, 0, 1), new Panel(PanelColor.GREEN, 3, 0, 2), new Panel(PanelColor.GRAY, 4, 0, 3), new Panel(PanelColor.GREEN, 5, 0, 4)],
+            [new Panel(PanelColor.GRAY, 6, 1, 0), new Panel(PanelColor.RED, 7, 1, 1), new Panel(PanelColor.RED, 8, 1, 2), new Panel(PanelColor.RED, 9, 1, 3), new Panel(PanelColor.GRAY, 10, 1, 4)],
+            [new Panel(PanelColor.GREEN, 11, 2, 0), new Panel(PanelColor.RED, 12, 2, 1), new Panel(PanelColor.RED, 13, 2, 2), new Panel(PanelColor.RED, 14, 2, 3), new Panel(PanelColor.GREEN, 15, 2, 4)],
+            [new Panel(PanelColor.GRAY, 16, 3, 0), new Panel(PanelColor.RED, 17, 3, 1), new Panel(PanelColor.RED, 18, 3, 2), new Panel(PanelColor.RED, 19, 3, 3), new Panel(PanelColor.GRAY, 20, 3, 4)],
+            [new Panel(PanelColor.GREEN, 21, 4, 0), new Panel(PanelColor.GRAY, 22, 4, 1), new Panel(PanelColor.GREEN, 23, 4, 2), new Panel(PanelColor.GRAY, 24, 4, 3), new Panel(PanelColor.GREEN, 25, 4, 4)]
+        ];
+        const gameBoard = new GameBoard();
+        gameBoard.setBoard(initialLayout);
+        gameController.setGameBoard(gameBoard);
+
+        // Select green color and select the panel at the middle
+        gameController.selectColor(PanelColor.GREEN);
+        const newGameBoard = gameController.selectPanel(13);
+
+        // The panels between the selected panel and another green panel should have flipped to green
+        const expectedLayout = [
+            [PanelColor.GREEN, PanelColor.GRAY, PanelColor.GREEN, PanelColor.GRAY, PanelColor.GREEN],
+            [PanelColor.GRAY, PanelColor.GREEN, PanelColor.GREEN, PanelColor.GREEN, PanelColor.GRAY],
+            [PanelColor.GREEN, PanelColor.GREEN, PanelColor.GREEN, PanelColor.GREEN, PanelColor.GREEN],
+            [PanelColor.GRAY, PanelColor.GREEN, PanelColor.GREEN, PanelColor.GREEN, PanelColor.GRAY],
+            [PanelColor.GREEN, PanelColor.GRAY, PanelColor.GREEN, PanelColor.GRAY, PanelColor.GREEN]
+        ];
+        const newLayout = newGameBoard.getBoard().map(row => row.map(panel => panel.getColor()));
+        expect(newLayout).toEqual(expectedLayout);
     });
 });
--- ts-attack25/src/domain/GameController.ts
+++ ts-attack25/src/domain/GameController.ts
@@ -2,14 +2,73 @@ import GameBoard from './GameBoard';
 import ColorCounter from './ColorCounter';
 import { Panel, PanelColor } from './panel';
 
+type Direction = {
+    rowOffset: number;
+    columnOffset: number;
+}
+
 class GameController {
     gameBoard: GameBoard;
     colorCounters: ColorCounter[];
     selectedColor: PanelColor | null = null;
 
+    // 八方向を示すオフセット
+    private directions: Direction[] = [
+        { rowOffset: -1, columnOffset: -1 }, // 左上
+        { rowOffset: -1, columnOffset: 0 }, // 上
+        { rowOffset: -1, columnOffset: 1 }, // 右上
+        { rowOffset: 0, columnOffset: -1 }, // 左
+        { rowOffset: 0, columnOffset: 1 }, // 右
+        { rowOffset: 1, columnOffset: -1 }, // 左下
+        { rowOffset: 1, columnOffset: 0 }, // 下
+        { rowOffset: 1, columnOffset: 1 }  // 右下
+    ];
+
     constructor(gameBoard: GameBoard, colorCounters: ColorCounter[]) {
         this.gameBoard = gameBoard;
         this.colorCounters = colorCounters;
+    }
+
+    private isPositionValid(board: Panel[][], row: number, col: number): boolean {
+        return row >= 0 && row < board.length && col >= 0 && col < board[0].length && board[row][col] !== undefined;
+    }
+
+    private incrementPosition(row: number, col: number, dir: Direction): [number, number] {
+        return [row + dir.rowOffset, col + dir.columnOffset];
+    }
+
+    private findPanelsToFlip(board: Panel[][], startRow: number, startCol: number, dir: Direction): Panel[] {
+        let [row, col] = this.incrementPosition(startRow, startCol, dir);
+        const panelsToFlip: Panel[] = [];
+
+        while (this.isPositionValid(board, row, col)) {
+            const currentPanel = board[row][col];
+
+            if (currentPanel.getColor() == this.selectedColor) {
+                // Same color found, return the list
+                return panelsToFlip;
+            } else if (currentPanel.getColor() == PanelColor.GRAY) {
+                // Empty panel, stop looking in this direction
+                return [];
+            } else {
+                // Different color, add to list
+                panelsToFlip.push(currentPanel);
+            }
+
+            [row, col] = this.incrementPosition(row, col, dir);
+        }
+
+        return [];
+    }
+
+    private flipPanels(panels: Panel[]) {
+        if (this.selectedColor === null) {
+            throw new Error('No color has been selected');
+        }
+
+        for (const panel of panels) {
+            panel.setColor(this.selectedColor);
+        }
     }
 
     setGameBoard(gameBoard: GameBoard): void {
@@ -39,8 +98,18 @@ class GameController {
 
         // Clone the gameBoard and apply the color change
         const newGameBoard = new GameBoard(this.gameBoard.getBoard().length, this.gameBoard.getBoard()[0].length);
-        newGameBoard.setBoard(this.gameBoard.getBoard().map(row => row.map(panel => new Panel(panel.getColor(), panel.getNumber()))));
-        newGameBoard.changeColor(panelNumber, this.selectedColor);
+        newGameBoard.setBoard(this.gameBoard.getBoard().map(row => row.map(panel => new Panel(panel.getColor(), panel.getNumber(), panel.getRow(), panel.getColumn()))));
+
+        // Change the color of the selected panel
+        const selectedPanel = newGameBoard.getBoard()[panel.getRow()][panel.getColumn()];
+        selectedPanel.setColor(this.selectedColor);
+
+        const startRow = panel.getRow();
+        const startCol = panel.getColumn();
+        for (const dir of this.directions) {
+            const panelsToFlip = this.findPanelsToFlip(newGameBoard.getBoard(), startRow, startCol, dir);
+            this.flipPanels(panelsToFlip);
+        }
 
         // Increment the counter of the new panel's color
         const newColorCounter = this.colorCounters.find(counter => counter.getColor() === this.selectedColor);
