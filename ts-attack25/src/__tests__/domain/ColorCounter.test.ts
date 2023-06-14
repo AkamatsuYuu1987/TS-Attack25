f78d2a258e0d687c8ce684022d975f8a323d77bc
phantomboyA <phantomboyA@gmail.com>
2023-06-14 17:10:43 +0900

    カラーカウンタークラス：外部から数字を受け取れるように変更

--- ts-attack25/src/__tests__/domain/ColorCounter.test.ts
+++ ts-attack25/src/__tests__/domain/ColorCounter.test.ts
@@ -6,7 +6,7 @@ describe('ColorCounter', () => {
     let counter: ColorCounter;
 
     beforeEach(() => {
-        counter = new ColorCounter(PanelColor.RED, 'red'); // Pass PanelColor.RED and 'red' to the constructor
+        counter = new ColorCounter(PanelColor.RED, 'red', 0);
     });
 
     test('should initialize with count of 0', () => {
@@ -35,7 +35,16 @@ describe('ColorCounter', () => {
         expect(counter.getColor()).toBe(PanelColor.RED);
     });
 
-    test('should get correct color name', () => { // New test for getColorName method
+    test('should get correct color name', () => {
         expect(counter.getColorName()).toBe('red');
+    });
+
+    // New test for setCount method
+    test('should set the count correctly', () => {
+        counter.setCount(5);
+        expect(counter.getCount()).toBe(5);
+
+        counter.setCount(0);
+        expect(counter.getCount()).toBe(0);
     });
 });
--- ts-attack25/src/domain/ColorCounter.ts
+++ ts-attack25/src/domain/ColorCounter.ts
@@ -3,20 +3,20 @@ import { PanelColor } from './panel';
 
 class ColorCounter {
   private color: PanelColor;
-  private colorName: string;  // 追加されたプロパティ
+  private colorName: string;
   private count: number;
 
-  constructor(color: PanelColor, colorName: string) {  // 更新されたコンストラクタ
+  constructor(color: PanelColor, colorName: string, count: number) {
     this.color = color;
-    this.colorName = colorName;  // 新しいプロパティの値を設定
-    this.count = 0;
+    this.colorName = colorName;
+    this.count = count;
   }
 
   getColor(): PanelColor {
     return this.color;
   }
 
-  getColorName(): string {  // 新たなメソッド
+  getColorName(): string {
     return this.colorName;
   }
 
@@ -32,6 +32,10 @@ class ColorCounter {
 
   getCount(): number {
     return this.count;
+  }
+
+  setCount(count: number): void {
+    this.count = count;
   }
 }
 
