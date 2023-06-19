645b6c0b1ecbc3420875bd5883cbc1c3a0345ea1
phantomboyA <phantomboyA@gmail.com>
2023-06-19 09:52:01 +0900

    カラーカウンターボードクラスの作成、テストとクラス図の修正

---
+++ ts-attack25/src/domain/ColorCounterBoard.ts
@@ -0,0 +1,28 @@
+import ColorCounter from './ColorCounter';
+import { Panel } from './panel';
+
+class ColorCounterBoard {
+    private counters: ColorCounter[];
+
+    constructor(counters: ColorCounter[]) {
+        this.counters = counters;
+    }
+
+    public getCounters(): ColorCounter[] {
+        return this.counters;
+    }
+
+    public setCounters(newCounters: ColorCounter[]): void {
+        this.counters = newCounters;
+    }
+
+    updateColorCounters(panels: Panel[]): void {
+        this.counters.forEach(counter => {
+            // Count the panels of each color
+            const panelCount = panels.filter(panel => panel.getColor() === counter.getColor()).length;
+            counter.setCount(panelCount);
+        });
+    }
+}
+
+export default ColorCounterBoard;
