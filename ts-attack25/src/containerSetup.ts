a0c568b2597e466da0960f7ecf790c2de9307732
phantomboyA <phantomboyA@gmail.com>
2023-10-11 02:59:04 +0900

    new GameControllerをcreateGameController化

--- ts-attack25/src/containerSetup.ts
+++ ts-attack25/src/containerSetup.ts
@@ -2,9 +2,14 @@
 import "reflect-metadata";
 import { Container } from "inversify";
 import { TYPES } from "./types";
+import { GameControllerTypes } from "@/types/GameControllerTypes";
 import { Directions } from "@/domain/Directions";
 import { PanelsToFlip } from "@/domain/PanelsToFlip";
 import { Panel, PanelColor } from "@/domain/panel";
+import SelectPanelExecutor from "@/domain/SelectPanelExecutor";
+import GameController from "@/domain/GameController";
+import GameBoard from "@/domain/GameBoard";
+import ColorCounterBoard from "@/domain/ColorCounterBoard";
 
 // コンテナのセットアップ
 const container = new Container();
@@ -16,5 +21,14 @@ container.bind<PanelsToFlip>(TYPES.PanelsToFlip).toDynamicValue(context => {
     const selectedColor = context.container.get<PanelColor>(TYPES.SelectedColor);
     return new PanelsToFlip(directions, gameBoard, selectedPanel, selectedColor);
 });
+
+// GameControllerの依存関係のバインディング
+container.bind<SelectPanelExecutor>(GameControllerTypes.SelectPanelExecutor).to(SelectPanelExecutor);
+container.bind<GameController>(GameControllerTypes.GameController).toDynamicValue(context => {
+    const gameBoard = context.container.get<GameBoard>(GameControllerTypes.GameBoard);
+    const colorCounterBoard = context.container.get<ColorCounterBoard>(GameControllerTypes.ColorCounterBoard);
+    const selectPanelExecutor = context.container.get<SelectPanelExecutor>(GameControllerTypes.SelectPanelExecutor);
+    return new GameController(gameBoard, colorCounterBoard, selectPanelExecutor);
+})
 
 export { container };
