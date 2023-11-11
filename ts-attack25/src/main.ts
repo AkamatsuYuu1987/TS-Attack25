9b5a6e3b607e8e01ddb6c321fb0f42d6791fa3ef
phantomboyA <phantomboyA@gmail.com>
2023-11-12 05:19:10 +0900

    feat: アプリの初期化処理にアクションを追加

--- ts-attack25/src/main.ts
+++ ts-attack25/src/main.ts
@@ -3,3 +3,5 @@ import App from './App.vue'
 import store from '@/store/index';
 
 createApp(App).use(store).mount('#app');
+// ストアの初期化後、初期ドメインエンティティ生成アクションを実行
+store.dispatch('InitializationServiceModule/initializeApp'); 
