69b9c3c07ae2ad557c3c290512c4fe23dfbff017
phantomboyA <phantomboyA@gmail.com>
2023-11-12 09:16:32 +0900

    chore: InitializationServiceModuleを削除

--- ts-attack25/src/store/index.ts
+++ ts-attack25/src/store/index.ts
@@ -4,14 +4,12 @@ import { PanelClickAnimationServiceModule } from '@/store/modules/animation-serv
 import { ColorCounterBoardStoreModule } from '@/store/modules/color-counter-board-store';
 import { GameBoardStoreModule } from '@/store/modules/game-board-store';
 import { GameControllerStoreModule } from '@/store/modules/game-controller-store';
-import { InitializationServiceModule } from '@/store/modules/services/initialization-service-store';
 const store = createStore({
     modules: {
         PanelClickAnimationService: PanelClickAnimationServiceModule,
         ColorCounterBoardStoreModule: ColorCounterBoardStoreModule,
         GameBoardStoreModule: GameBoardStoreModule,
         GameControllerStoreModule: GameControllerStoreModule,
-        InitializationServiceModule: InitializationServiceModule
     }
 });
 
