f43a21003b317e434eca29669dc34b7fde3ff159
phantomboyA <phantomboyA@gmail.com>
2023-11-13 18:17:35 +0900

    test: gameControllerクラスのストアのテストを整理

--- ts-attack25/src/__tests__/store/modules/game-controller-store.spec.ts
+++ ts-attack25/src/__tests__/store/modules/game-controller-store.spec.ts
@@ -2,137 +2,95 @@
 import { GameControllerStoreModule } from '@/store/modules/game-controller-store';
 import GameBoard from '@/domain/GameBoard';
 import ColorCounterBoard from '@/domain/ColorCounterBoard';
-import { PanelColor } from '@/domain/panel';
 import ColorCounterType from '@/domain/ColorCounter';
-import { createGameController } from '@/factories/gameControllerFactory'
-
-const defaultState = GameControllerStoreModule.state;
+import { PanelColor } from '@/domain/panel';
+import { createGameController } from '@/factories/gameControllerFactory';
+import { createStore, Store } from 'vuex';
 
 describe('GameControllerStore Vuex Module', () => {
+    let store: Store<any>;
+    let newGameController: ReturnType<typeof createGameController>;
 
-    const colorCounters = [
-        new ColorCounterType(PanelColor.RED, 'red', 5),
-        new ColorCounterType(PanelColor.GREEN, 'green', 4),
-        new ColorCounterType(PanelColor.BLUE, 'blue', 3),
-        new ColorCounterType(PanelColor.WHITE, 'white', 2),
-    ];
-    // colorCounterBoardをコンストラクタから生成
-    const colorCounterBoard = new ColorCounterBoard(colorCounters);
+    beforeEach(() => {
+        store = createStore({
+            modules: {
+                GameControllerStoreModule: GameControllerStoreModule
+            }
+        });
+        newGameController = createGameController(new GameBoard(5, 5), new ColorCounterBoard([
+            new ColorCounterType(PanelColor.RED, 'red', 5),
+            new ColorCounterType(PanelColor.GREEN, 'green', 4),
+            new ColorCounterType(PanelColor.BLUE, 'blue', 3),
+            new ColorCounterType(PanelColor.WHITE, 'white', 2),
+        ]));
+        store.commit('GameControllerStoreModule/SET_GAME_CONTROLLER', newGameController);
+    });
 
     describe('mutations', () => {
         it('SET_GAME_CONTROLLER should set game controller', () => {
-            const state = {
-                ...(typeof defaultState === 'function' ? defaultState() : defaultState),
-                gameController: null
-            };
-
-            const newGameController = createGameController(new GameBoard(5, 5), colorCounterBoard,);
-
-            GameControllerStoreModule.mutations!.SET_GAME_CONTROLLER(state, newGameController);
-
+            const state = store.state.GameControllerStoreModule;
             expect(state.gameController).toEqual(newGameController);
         });
 
         it('UPDATE_GAME_BOARD should update game board', () => {
-            const gameBoard = new GameBoard(5, 5);
-            const gameController = createGameController(gameBoard, colorCounterBoard);
-            const state = {
-                ...(typeof defaultState === 'function' ? defaultState() : defaultState),
-                gameController
-            };
-
             const newGameBoard = new GameBoard(3, 3);
-            GameControllerStoreModule.mutations!.UPDATE_GAME_BOARD(state, newGameBoard);
-
+            store.commit('GameControllerStoreModule/UPDATE_GAME_BOARD', newGameBoard);
+            const state = store.state.GameControllerStoreModule;
             expect(state.gameController?.gameBoard).toEqual(newGameBoard);
         });
 
         it('UPDATE_COLOR_COUNTER_BOARD should update color counter board', () => {
-            const gameBoard = new GameBoard(5, 5);
-            const gameController = createGameController(gameBoard, colorCounterBoard);
-            const state = {
-                ...(typeof defaultState === 'function' ? defaultState() : defaultState),
-                gameController
-            };
-
-            const newColorCounters = [
-                new ColorCounterType(PanelColor.RED, 'red', 5),
-                new ColorCounterType(PanelColor.GREEN, 'green', 4),
+            const newColorCounterBoard = new ColorCounterBoard([
+                new ColorCounterType(PanelColor.RED, 'red', 1),
+                new ColorCounterType(PanelColor.GREEN, 'green', 2),
                 new ColorCounterType(PanelColor.BLUE, 'blue', 3),
-                new ColorCounterType(PanelColor.WHITE, 'white', 2),
-            ];
-            const newColorCounterBoard = new ColorCounterBoard(newColorCounters);
-            GameControllerStoreModule.mutations!.UPDATE_COLOR_COUNTER_BOARD(state, newColorCounterBoard);
-
+                new ColorCounterType(PanelColor.WHITE, 'white', 4),
+            ]);
+            store.commit('GameControllerStoreModule/UPDATE_COLOR_COUNTER_BOARD', newColorCounterBoard);
+            const state = store.state.GameControllerStoreModule;
             expect(state.gameController?.colorCounterBoard).toEqual(newColorCounterBoard);
         });
     });
 
     describe('actions', () => {
         it('setGameController should commit SET_GAME_CONTROLLER', async () => {
-            const commit = jest.fn();
-            const newGameController = createGameController(new GameBoard(5, 5), colorCounterBoard);
-
-            // 型アサーションを使用して、関数として呼び出す
-            await (GameControllerStoreModule.actions!.setGameController as any)({ commit }, newGameController);
-
-            expect(commit).toHaveBeenCalledWith('SET_GAME_CONTROLLER', newGameController);
+            await store.dispatch('GameControllerStoreModule/setGameController', newGameController);
+            expect(store.state.GameControllerStoreModule.gameController).toEqual(newGameController);
         });
 
         it('updateGameBoard should commit UPDATE_GAME_BOARD', async () => {
-            const commit = jest.fn();
-            const newGameBoard = new GameBoard(5, 5);
-
-            // 型アサーションを使用して、関数として呼び出す
-            await (GameControllerStoreModule.actions!.updateGameBoard as any)({ commit }, newGameBoard);
-
-            expect(commit).toHaveBeenCalledWith('UPDATE_GAME_BOARD', newGameBoard);
+            await store.dispatch('GameControllerStoreModule/setGameController', newGameController);
+            const newGameBoard = new GameBoard(3, 3);
+            await store.dispatch('GameControllerStoreModule/updateGameBoard', newGameBoard);
+            expect(store.state.GameControllerStoreModule.gameController?.gameBoard).toEqual(newGameBoard);
         });
 
         it('updateColorCounterBoard should commit UPDATE_COLOR_COUNTER_BOARD', async () => {
-            const commit = jest.fn();
-            const newColorCounters = [
-                new ColorCounterType(PanelColor.RED, 'red', 5),
-                new ColorCounterType(PanelColor.GREEN, 'green', 4),
+            await store.dispatch('GameControllerStoreModule/setGameController', newGameController);
+            const newColorCounterBoard = new ColorCounterBoard([
+                new ColorCounterType(PanelColor.RED, 'red', 1),
+                new ColorCounterType(PanelColor.GREEN, 'green', 2),
                 new ColorCounterType(PanelColor.BLUE, 'blue', 3),
-                new ColorCounterType(PanelColor.WHITE, 'white', 2),
-            ];
-            const newColorCounterBoard = new ColorCounterBoard(newColorCounters);
-
-            // 型アサーションを使用して、関数として呼び出す
-            await (GameControllerStoreModule.actions!.updateColorCounterBoard as any)({ commit }, newColorCounterBoard);
-
-            expect(commit).toHaveBeenCalledWith('UPDATE_COLOR_COUNTER_BOARD', newColorCounterBoard);
+                new ColorCounterType(PanelColor.WHITE, 'white', 4),
+            ]);
+            await store.dispatch('GameControllerStoreModule/updateColorCounterBoard', newColorCounterBoard);
+            expect(store.state.GameControllerStoreModule.gameController?.colorCounterBoard).toEqual(newColorCounterBoard);
         });
     });
 
     describe('getters', () => {
         it('gameBoardGetter should return game board', () => {
-            const gameBoard = new GameBoard(5, 5);
-            const gameController = createGameController(gameBoard, colorCounterBoard);
-
-            const state = {
-                ...(typeof defaultState === 'function' ? defaultState() : defaultState),
-                gameController
-            };
-
-            const result = GameControllerStoreModule.getters!.gameBoardGetter(state, {}, {}, {});
-
-            expect(result).toEqual(gameBoard);
+            const state = store.state.GameControllerStoreModule;
+            const getter = GameControllerStoreModule.getters!.gameBoardGetter as any;
+            const result = getter(state);
+            expect(result).toEqual(newGameController.gameBoard);
         });
 
         it('colorCounterBoardGetter should return color counter board', () => {
-            const gameBoard = new GameBoard(5, 5);
-            const gameController = createGameController(gameBoard, colorCounterBoard);
-
-            const state = {
-                ...(typeof defaultState === 'function' ? defaultState() : defaultState),
-                gameController
-            };
-
-            const result = GameControllerStoreModule.getters!.colorCounterBoardGetter(state, {}, {}, {});
-
-            expect(result).toEqual(colorCounterBoard);
+            const state = store.state.GameControllerStoreModule;
+            const getter = GameControllerStoreModule.getters!.colorCounterBoardGetter as any;
+            const result = getter(state);
+            expect(result).toEqual(newGameController.colorCounterBoard);
         });
     });
 });
